66b76c223092ab99f010b65b37aea51f6ad4c369
Luis Osvaldo <aldolp40@gmail.com>
2019-05-30 11:04:17 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Logorma from '../assets/logo-rma.png';
 import '../css/Navbar.css';
 import {
@@ -49,85 +50,117 @@ export default class CustomNavbar extends React.Component {
     return (
       <div className="prevent-nav">
         <Navbar color="#fff" light expand="md" className="custom-navbar">
-          <NavbarBrand href="/"><img src={Logorma} alt="logo-rma" /></NavbarBrand>
+          <Link to="/"><img src={Logorma} alt="logo-rma" /></Link>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
               <NavItem className="navItem">
-                <NavLink href="/" className="navCont"> {this.props.data.navbar.home.inicio} </NavLink>
-              </NavItem>
-              <NavItem className="navItem">
-                <NavLink href="/quienessomos">
-                  <FontAwesomeIcon className="i" icon="users" />
-                  {this.props.data.navbar.home.inicio}
+                <NavLink>
+                  <Link className="navCont" to="/">
+                    {this.props.data.navbar.home.content}
+                  </Link>
                 </NavLink>
               </NavItem>
               <NavItem className="navItem">
-                <NavLink href="/abogados">
-                  <FontAwesomeIcon className="i" icon="gavel" />
-                  {this.props.data.navbar.home.inicio}
+                <NavLink>
+                  <Link className="navCont" to="/quienessomos">
+                    <FontAwesomeIcon className="i" icon="users" />
+                    {this.props.data.navbar.quienesSomos.content}
+                  </Link>
+                </NavLink>
+              </NavItem>
+              <NavItem className="navItem">
+                <NavLink >
+                  <Link className="navCont" to="/abogados">
+                    <FontAwesomeIcon className="i" icon="gavel" />
+                    {this.props.data.navbar.abogados.content}
+                  </Link>
                 </NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar className="navItem">
                 <DropdownToggle nav caret>
-                  <FontAwesomeIcon className="i" icon="home" />
-                  {this.props.data.navbar.home.inicio}
+                  <Link className="navCont">
+                    <FontAwesomeIcon className="i" icon="home" />
+                    {this.props.data.navbar.areasDePractica.content}
+                  </Link>
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem href="/telecomunicaciones">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/creditos-finanzas">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/radiodifusion">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/derecho-corporativo">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/energia">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/competencia-economica">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/asesoria-legislativa">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
-                  <DropdownItem href="/litigio">
-                    <FontAwesomeIcon className="i" icon="caret-right" />
-                    {this.props.data.navbar.home.inicio}
-                  </DropdownItem>
+                  <Link to="/telecomunicaciones">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.telecom.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/creditos-finanzas">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.creditos.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/radiodifusion">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.radio.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/derecho-corporativo">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.transaccional.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/energia">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.energia.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/competencia-economica">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.competenciaEconomica.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/asesoria-legislativa">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.asesoria.content}
+                    </DropdownItem>
+                  </Link>
+                  <Link to="/litigio">
+                    <DropdownItem className="navCont">
+                      <FontAwesomeIcon className="i" icon="caret-right" />
+                      {this.props.data.navbar.litigio.content}
+                    </DropdownItem>
+                  </Link>
                 </DropdownMenu>
               </UncontrolledDropdown>
               <NavItem className="navItem">
-                <NavLink href="/bolsadetrabajo">
-                  <FontAwesomeIcon className="i" icon="suitcase" />
-                  {this.props.data.navbar.home.inicio}
+                <NavLink>
+                  <Link className="navCont" to="/bolsadetrabajo">
+                    <FontAwesomeIcon className="i" icon="suitcase" />
+                    {this.props.data.navbar.bolsaDeTrabajo.content}
+                  </Link>
                 </NavLink>
               </NavItem>
               <NavItem className="navItem">
-                <NavLink href="/publicaciones">
-                  <FontAwesomeIcon className="i" icon="file-contract" />
-                  {this.props.data.navbar.home.inicio}
+                <NavLink>
+                  <Link className="navCont" to="/publicaciones">
+                    <FontAwesomeIcon className="i" icon="file-contract" />
+                    {this.props.data.navbar.publicaciones.content}
+                  </Link>
                 </NavLink>
               </NavItem>
               <NavItem className="navItem">
-                <NavLink href="/contacto">
-                  <FontAwesomeIcon className="i" icon="phone" />
-                  {this.props.data.navbar.home.inicio}
+                <NavLink>
+                  <Link className="navCont" to="/contacto">
+                    <FontAwesomeIcon className="i" icon="phone" />
+                    {this.props.data.navbar.contacto.content}
+                  </Link>
                 </NavLink>
               </NavItem>
               <NavItem className="navItem">
-                <img style={{width: '45px'}} onChange={this.props.handelChangeIdioma} src={imagesPath[imageName]} onClick={this.toggleImage} />
+                <img style={{ width: '45px' }} onChange={this.props.handelChangeIdioma} src={imagesPath[imageName]} onClick={this.toggleImage} />
               </NavItem>
             </Nav>
           </Collapse>
